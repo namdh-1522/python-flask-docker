509ce679602872a07503d94fbeca95cb5c2596bf
duong.huu.nam <duong.huu.nam@b121522-mb.local>
2022-09-21 17:05:43 +0700

    Using ENV

--- web/app.py
+++ web/app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+import os
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Flask hehe!!'
+    return f'Hello Flask. ENV Value = {os.getenv("TEST_ENV")}'
 
 if __name__ == '__main__':
     app.run(debug = True, host = '0.0.0.0')
